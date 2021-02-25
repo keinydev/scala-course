1da49eaea889d7d4f2c4d27d015c00b5ec532905
Keiny Pacheco Cárcamo <tatianapacheco@seven4n.com>
2021-02-25 18:43:17 -0500

    Modify test

--- src/test/scala/co/s4n/inmutable/list/ListSpec.scala
+++ src/test/scala/co/s4n/inmutable/list/ListSpec.scala
@@ -4,7 +4,8 @@ import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
 class ListSpec extends AnyFlatSpec with Matchers {
-  "El ejercicio 1 " should " 9" in {
+
+  "El ejercicio 1 " should " be 9" in {
     val x = List(4,5,6,7,8) match{
       case Const(x, Const(5, Const(7,_))) =>x
       case Nil => 1
@@ -14,6 +15,28 @@ class ListSpec extends AnyFlatSpec with Matchers {
     }
     val y = List
     x shouldEqual + 9
+  }
+
+  "El ejercicio 2 " should " remover primer item" in {
+    val lst1 = List("a","b","c","d")
+    val lst2 = List("b","c","d")
+    List.tail(lst1) shouldEqual lst2
+  }
+
+  "El ejercicio 3 " should " mostrar primer item" in {
+    val lst = List("a","b","c","d")
+    val head = "a"
+    List.head(lst) shouldEqual head
+  }
+
+  "El ejercicio 4 " should "  true si todo es verdadero" in {
+    val lst = List(true,true,true,true,true)
+    List.and(lst) shouldEqual true
+  }
+
+  "El ejercicio 4 " should "  false si no todo es verdadero" in {
+    val lst = List(true,true,false,true,true)
+    List.and(lst) shouldEqual false
   }
 }
 
